4f9ce1ed18a94c685683bc12f693420bec6dbe78
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-12 00:27:30 -0400

    Remove index.ts and nodemon

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { authorize, createGmailClient, listLabels, listThreads } from "./utils/gmail";
-
-authorize()
-  .then(createGmailClient)
-  .then(async (gmail) => {
-    await listLabels(gmail);
-    await listThreads(gmail, ["INBOX"]);
-  })
-  .catch(console.error);
