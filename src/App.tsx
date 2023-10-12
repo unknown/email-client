380fc61ba68f900b1be168fd3005cf20f95adb0c
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-12 10:18:06 -0400

    Add Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [labels, setLabels] = useState<string[]>([]);
+  const [labels, setLabels] = useState<string[]>();
 
   async function updateLabels() {
     const labels = (await window.gmail.getLabels()) ?? [];
@@ -10,14 +10,19 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Electron + Vite + React</h1>
-      <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+    <div className="space-y-3 p-4">
+      <h1 className="underline">email client</h1>
+      <button
+        className="px-2 py-3 bg-gray-200 rounded-lg"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        count is {count}
+      </button>
       <div>
-        <button onClick={updateLabels}>get labels</button>
-        {labels.map((label, i) => (
-          <div key={i}>{label}</div>
-        ))}
+        <button className="px-2 py-3 bg-gray-200 rounded-lg" onClick={updateLabels}>
+          get labels
+        </button>
+        {labels?.map((label, i) => <div key={i}>{label}</div>)}
       </div>
     </div>
   );
