a9717e752f7f3a06381c8ab47169a6d6ab86a7f6
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-13 01:03:58 -0400

    Display thread header data (#6)
    * Display from and to addresses in email preview
    
    * Add cc and reply-to headers

--- src/components/email-preview.tsx
+++ src/components/email-preview.tsx
@@ -1,24 +1,54 @@
-import { DecodedMessage } from "../utils/decoder";
+import { gmail_v1 } from "googleapis";
+
+import { decodePayload } from "../utils/decoder";
 
 type EmailPreviewProps = {
-  decodedMessage: DecodedMessage;
+  message: gmail_v1.Schema$Message;
 };
 
-export function EmailPreview({ decodedMessage: { text, html } }: EmailPreviewProps) {
+export function EmailPreview({ message }: EmailPreviewProps) {
+  const { html, text, headers } = decodePayload(message.payload);
+  const date = message.internalDate ? new Date(parseInt(message.internalDate)) : null;
+  const dateString = date?.toLocaleString(undefined, { dateStyle: "medium", timeStyle: "short" });
+
   let htmlToRender: string | null = null;
   if (html !== null) {
     htmlToRender = html;
   } else if (text !== null) {
     const textWithBr = text.replaceAll("\n", "<br />");
     htmlToRender = textWithBr;
+  } else {
+    htmlToRender = "Email could not be decoded";
   }
 
-  if (htmlToRender === null) {
-    return "Email could not be decoded";
-  }
+  const from = headers["From"];
+  const to = headers["Delivered-To"] ?? headers["To"];
+  const cc = headers["Cc"];
+  const replyTo = headers["Reply-To"];
 
   return (
-    <div className="outline p-4 outline-gray-200">
+    <div className="flex flex-col gap-4 outline p-4 outline-gray-200">
+      <div className="flex justify-between">
+        <div>
+          <div>
+            From: <span className="text-gray-600">{from}</span>
+          </div>
+          <div>
+            To: <span className="text-gray-600">{to}</span>
+          </div>
+          {cc && (
+            <div>
+              Cc: <span className="text-gray-600">{cc}</span>
+            </div>
+          )}
+          {replyTo && (
+            <div>
+              Reply To: <span className="text-gray-600">{replyTo}</span>
+            </div>
+          )}
+        </div>
+        <div>{dateString}</div>
+      </div>
       <div dangerouslySetInnerHTML={{ __html: htmlToRender }} />
     </div>
   );
