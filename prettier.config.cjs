7bbea6e6bc93637df4461c8ed1b82adcf1824981
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-14 10:19:34 -0400

    Add `prettier-plugin-tailwindcss`

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,14 +1,15 @@
 /** @typedef  {import("prettier").Config} PrettierConfig*/
 /** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
+/** @typedef  {import("prettier-plugin-tailwindcss").PluginOptions} TailwindPluginConfig*/
 
-/** @type { PrettierConfig | SortImportsConfig } */
+/** @type { PrettierConfig | SortImportsConfig | TailwindPluginConfig } */
 const config = {
   printWidth: 100,
   singleQuote: false,
   tabWidth: 2,
   semi: true,
   trailingComma: "all",
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
   importOrder: ["<THIRD_PARTY_MODULES>", "", "^@/(.*)$", "^[./]"],
 };
 
