81b98b98f581eb1d9ace1e91716b97a52ebafaa2
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-15 10:39:13 -0400

    Rename Tailwind plugin to `unpreflight`

--- unpreflight.plugin.cjs
+++ unpreflight.plugin.cjs
@@ -2,20 +2,25 @@ const plugin = require("tailwindcss/plugin");
 const fs = require("fs");
 const postcss = require("postcss");
 
-module.exports = plugin(function ({ addBase }) {
+module.exports = plugin(function ({ addBase, addUtilities }) {
   const preflightStyles = postcss.parse(
     fs.readFileSync(require.resolve("tailwindcss/lib/css/preflight.css"), "utf8"),
   );
 
   preflightStyles.walkRules((rule) => {
-    const selectors = rule.selector.split(",");
-    rule.selector = selectors.map((selector) => `.email-preview ${selector}`).join(",");
+    rule.selectors = rule.selectors.map((selector) => `.unpreflight ${selector}`);
+    rule.selector = rule.selectors.join(",");
     rule.nodes.forEach((node, i) => {
-      if (node.type == "decl") {
+      if (node.type === "decl") {
         rule.nodes[i].value = "revert";
       }
     });
   });
 
   addBase(preflightStyles.nodes);
+  addUtilities({
+    ".unpreflight": {
+      all: "revert",
+    },
+  });
 });
