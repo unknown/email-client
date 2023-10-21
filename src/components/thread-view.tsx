2d6bb1c8661a97d289e82687c5100954795cd8ba
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-21 12:06:06 -0400

    Add email summarization feature

--- src/components/thread-view.tsx
+++ src/components/thread-view.tsx
@@ -1,4 +1,7 @@
+import { Fragment, useState } from "react";
+
 import { EmailThread } from "@/electron/gmail/types";
+import { summarizeThread } from "@/utils/openai";
 import { EmailMessage } from "./email-message";
 
 type ThreadViewProps = {
@@ -6,6 +9,9 @@ type ThreadViewProps = {
 };
 
 export function ThreadView({ thread }: ThreadViewProps) {
+  const [summary, setSummary] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   if (!thread) {
     return (
       <div className="flex items-center">
@@ -15,11 +21,41 @@ export function ThreadView({ thread }: ThreadViewProps) {
   }
 
   const latestMessage = thread.messages.at(-1);
-  const subject = latestMessage ? latestMessage.decodedPayload.headers["Subject"] : null;
+  if (!latestMessage) {
+    return (
+      <div className="flex items-center">
+        <p className="w-full text-center">Empty email thread</p>
+      </div>
+    );
+  }
+
+  const subject = latestMessage.decodedPayload.headers["Subject"];
 
   return (
-    <div className="overflow-scroll px-6 py-4">
+    <div className="flex flex-col gap-2 overflow-scroll px-6 py-4">
       {subject && <h1 className="text-lg font-bold">{subject}</h1>}
+      <div className="flex flex-wrap items-center justify-center gap-3 rounded-md bg-purple-50 px-4 py-2 sm:flex-nowrap sm:justify-normal">
+        {summary ? (
+          <div>{summary}</div>
+        ) : (
+          <Fragment>
+            <p>Get an AI-powered summary of this email thread?</p>
+            <button
+              onClick={async () => {
+                setIsLoading(true);
+                const response = await summarizeThread(thread);
+                setSummary(response.choices.at(0)?.message.content ?? "Error");
+                setIsLoading(false);
+                console.log(response);
+              }}
+              disabled={isLoading || summary !== null}
+              className="rounded-md bg-purple-100 px-3 py-2 text-sm"
+            >
+              Generate
+            </button>
+          </Fragment>
+        )}
+      </div>
       <div className="flex flex-col divide-y">
         {thread.messages?.map((message, i) => {
           return <EmailMessage key={message.id ?? i} message={message} />;
