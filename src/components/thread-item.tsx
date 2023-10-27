9875980edbdf419fb56bff49356661b371ca9c7b
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-26 23:20:41 -0400

    Add unread email indicator (#22)
    * Add unread email indicator
    
    * Remove redundant style

--- src/components/thread-item.tsx
+++ src/components/thread-item.tsx
@@ -14,6 +14,7 @@ export function ThreadItem({ thread, isSelected, onThreadClick }: ThreadItemProp
   const senders = thread.messages.map((message) => message.decodedPayload.headers["From"]);
   const subject = lastMessage?.decodedPayload.headers["Subject"];
   const snippet = lastMessage?.snippet;
+  const isUnread = lastMessage?.labelIds?.includes("UNREAD");
 
   const uniqueSenders = [...new Set(senders)];
   const sendersText = uniqueSenders
@@ -23,17 +24,27 @@ export function ThreadItem({ thread, isSelected, onThreadClick }: ThreadItemProp
   return (
     <div
       className={twMerge(
-        "relative break-words px-4 py-2 text-sm",
+        "relative flex break-words px-4 py-2 text-sm",
         isSelected ? "text-white" : null,
       )}
       onClick={onThreadClick}
     >
-      <div
-        className={twMerge("absolute inset-0 -z-10 rounded-md", isSelected ? "bg-blue-600" : null)}
-      />
-      <h2 className="truncate font-bold">{sendersText}</h2>
-      <h3 className="truncate font-medium">{subject}</h3>
-      <p className={twMerge("truncate", !isSelected ? "text-tx-2" : null)}>{snippet}</p>
+      {isSelected && <div className={twMerge("absolute inset-0 -z-10 rounded-md bg-blue-600")} />}
+      <div className="relative w-4 flex-shrink-0">
+        {isUnread && (
+          <div
+            className={twMerge(
+              "absolute top-1/2 h-2 w-2 -translate-x-1/2 -translate-y-1/2 rounded-full",
+              isSelected ? "bg-white" : "bg-blue-600",
+            )}
+          />
+        )}
+      </div>
+      <div className="min-w-0">
+        <h2 className="truncate font-bold">{sendersText}</h2>
+        <h3 className="truncate font-medium">{subject}</h3>
+        <p className={twMerge("truncate", !isSelected ? "text-tx-2" : null)}>{snippet}</p>
+      </div>
     </div>
   );
 }
