c867ac5cc01b6658996c132fd5d8062c95298323
David Mo <44306450+unknown@users.noreply.github.com>
2023-10-30 16:18:53 -0400

    Fetch latest emails in `listInbox`

--- electron/gmail/api.ts
+++ electron/gmail/api.ts
@@ -35,52 +35,72 @@ export async function getThread(id: string) {
   return res.data;
 }
 
+interface SavedInbox {
+  lastUpdated: number;
+  threads: EmailThread[];
+}
+
 async function loadSavedInboxIfExist() {
   try {
     // TODO: better type-checking
     const content = await fs.readFile(INBOX_PATH, { encoding: "utf-8" });
-    const threads = JSON.parse(content).threads as EmailThread[];
-    return threads;
+    const inbox = JSON.parse(content) as SavedInbox;
+    return inbox;
   } catch (err) {
     return null;
   }
 }
 
-async function saveInbox(threads: EmailThread[]) {
-  const payload = JSON.stringify({ threads }, null, 2);
+async function saveInbox(inbox: SavedInbox) {
+  const payload = JSON.stringify(inbox, null, 2);
   await fs.writeFile(INBOX_PATH, payload);
 }
 
 export async function listInbox() {
-  const savedThreads = await loadSavedInboxIfExist();
-  if (savedThreads) {
-    return savedThreads;
-  }
+  const savedInbox = await loadSavedInboxIfExist();
+  const isFirstSync = savedInbox === null;
+  const threads = savedInbox?.threads ?? [];
 
   const gmail = await getGmailClient();
-  const threads: EmailThread[] = [];
   let nextPageToken: string | null = null;
+  const afterEpochSeconds = savedInbox?.lastUpdated ?? null;
+  const beforeEpochSeconds = Math.ceil(Date.now() / 1000);
+  const query = `${
+    afterEpochSeconds ? `after:${afterEpochSeconds}` : ""
+  } before:${beforeEpochSeconds}`;
 
   do {
     const res = await gmail.users.threads.list({
       labelIds: ["INBOX"],
       pageToken: nextPageToken ?? undefined,
+      q: query ?? undefined,
       maxResults: 20,
       userId: "me",
     });
-    const newThreads: EmailThread[] = new Array(res.data.threads?.length);
-    await Promise.all(
-      res.data.threads?.map(async ({ id }, i) => {
+
+    const newThreads = res.data.threads ?? [];
+    const decodedThreads = await Promise.all(
+      newThreads.map(async ({ id }) => {
         if (id) {
           const thread = await getThread(id);
-          newThreads[i] = decodeEmailThread(thread);
+          return decodeEmailThread(thread);
         }
-      }) ?? [],
+      }),
     );
-    threads.push(...newThreads);
-    nextPageToken = (res.data.nextPageToken ?? null) as string | null;
-  } while (nextPageToken !== null && threads.length < 20);
+    decodedThreads.forEach((thread) => thread !== undefined && threads.push(thread));
 
-  saveInbox(threads);
+    nextPageToken = (res.data.nextPageToken ?? null) as string | null;
+  } while (nextPageToken !== null && (!isFirstSync || threads.length < 20));
+
+  threads.sort((a, b) => {
+    const id1 = parseInt(a.historyId ?? "");
+    const id2 = parseInt(b.historyId ?? "");
+    return id2 - id1;
+  });
+
+  saveInbox({
+    lastUpdated: beforeEpochSeconds,
+    threads,
+  });
   return threads;
 }
