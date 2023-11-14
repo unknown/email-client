162001a7b2f46e24cbca5819a381d6b959b2b302
David Mo <44306450+unknown@users.noreply.github.com>
2023-11-14 15:46:49 -0500

    Use square brackets when accessing record

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -15,8 +15,8 @@ export function getNameAndEmail(emailStr: string) {
   const regex = /^(?:(?<name>.*)\s)?\<(?<email>.*)\>$/;
   const groups = emailStr.match(regex)?.groups;
 
-  const name = removeQuotes(groups?.name);
-  const email = groups?.email;
+  const name = removeQuotes(groups?.["name"]);
+  const email = groups?.["email"];
 
   return { name, email };
 }
